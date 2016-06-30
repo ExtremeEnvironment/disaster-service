49c34108c5acc6a43eaa5910026ab07061ec3959
David Steiman <d.steiman@stytex.de>
2016-06-30 14:32:30 +0200

    adds feign client integration with test profile separation

---
+++ src/main/java/de/extremeenvironment/disasterservice/config/FeignConfiguration.java
@@ -0,0 +1,16 @@
+package de.extremeenvironment.disasterservice.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by on 30.06.16.
+ *
+ * @author David Steiman
+ */
+@Configuration
+@Profile("!test")
+@EnableFeignClients
+public class FeignConfiguration {
+}
---
+++ src/main/java/de/extremeenvironment/disasterservice/config/LoadBalancedResourceDetails.java
@@ -0,0 +1,61 @@
+package de.extremeenvironment.disasterservice.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+@Component
+public class LoadBalancedResourceDetails extends ClientCredentialsResourceDetails {
+    
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public LoadBalancedResourceDetails(JHipsterProperties jHipsterProperties) {
+        this.jHipsterProperties = jHipsterProperties;
+
+        setAccessTokenUri(jHipsterProperties.getSecurity().getClientAuthorization().getTokenUrl());
+        setClientId(jHipsterProperties.getSecurity().getClientAuthorization().getClientId());
+        setClientSecret(jHipsterProperties.getSecurity().getClientAuthorization().getClientSecret());
+        setGrantType("client_credentials");
+
+    }
+
+    private LoadBalancerClient loadBalancerClient;
+
+    private JHipsterProperties jHipsterProperties;
+
+
+    @Autowired(required = false)
+    public void setLoadBalancerClient(LoadBalancerClient loadBalancerClient) {
+        this.loadBalancerClient = loadBalancerClient;
+    }
+
+    @Override
+    public String getAccessTokenUri() {
+        String serviceName = jHipsterProperties.getSecurity().getClientAuthorization().getTokenServiceId();
+        if (loadBalancerClient != null && !serviceName.isEmpty()) {
+            String newUrl;
+            try {
+                newUrl = loadBalancerClient.reconstructURI(
+                    loadBalancerClient.choose(serviceName),
+                    new URI(super.getAccessTokenUri())
+                ).toString();
+
+                return newUrl;
+            } catch (URISyntaxException e) {
+                log.error("{}: {}", e.getClass().toString(), e.getMessage());
+
+                return super.getAccessTokenUri();
+            }
+        } else {
+            return super.getAccessTokenUri();
+        }
+    }
+}
